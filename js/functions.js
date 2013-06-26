fa62c65c30e7e80961ee9a34122131b2033f5ace
Thierry Poinot <poinot.thierry@gmail.com>
2013-06-26 17:47:25 +0200

    better placeholder
    remove the placeholder when input or textarea has focus
    restore the placeholder if the value is empty

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,34 @@
-// Get a unique id
+/**
+ * Get a unique id
+ */
 function uID () { 
 	return new Date().getTime();
 }
+
+/**
+ * IIFE
+ */
+(function(w, d){
+	
+})(window, document)
+
+
+/**
+ * Better placeholder, need jquery
+ * remove the placeholder when input or textarea has focus
+ * restore the placeholder if the value is empty
+ */
+$('form input[type="text"], form textarea')
+	.bind('focus', function(){
+            if ($(this).val() == '') {
+                $(this).attr('placeholder', '');
+                $(this).prop('placeholder', '');
+            }
+        })
+        .bind('focusout', function(){
+            if ($(this).val() == '') {
+                $(this).attr('placeholder', $(this).data('placeholder'));
+                $(this).prop('placeholder', $(this).data('placeholder'));
+            }
+        })
+;
