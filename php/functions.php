833af77550e2c75c298f90eaf33095b4657ffc76
Thierry Poinot <poinot.thierry@gmail.com>
2013-06-27 19:39:33 +0200

    Update functions.php

--- php/functions.php
+++ php/functions.php
@@ -1,4 +1,55 @@
 /**
+ * 
+ * @param  [type] $__name          [description]
+ * @param  [type] $__default       [description]
+ * @param  string $__whereToSearch GET|POST|SERVER
+ * @return [type]                  [description]
+ */
+function _get_global($__name, $__default = null, $__whereToSearch = null) {
+    $globalsVar = array(
+        'post' => $_POST,
+        'get' => $_GET,
+        'server' => $_SERVER,
+    );
+    if ($__whereToSearch == null) {
+
+        foreach ($globalsVar as $search) {
+            if (is_array($search)) {
+                if (array_key_exists($__name, $search)) {
+                    return $search[$__name];
+                }
+            }
+        }
+    }
+    else {
+        switch($__whereToSearch) {
+            case 'GET':
+            case 'get':
+            case 'g':
+                $search = $globalsVar['get'];
+                break;
+            case 'POST':
+            case 'post':
+            case 'p':
+                $search = $globalsVar['post'];
+                break;
+            case 'SERVER':
+            case 'server':
+            case 's':
+                $search = $globalsVar['server'];
+                break;
+        }
+        if (is_array($search)) {
+            if (array_key_exists($__name, $search)) {
+                return $search[$__name];
+            }
+        }
+    }
+    
+    return $__default;
+}
+
+/**
  * @author  Thierry Poinot - Les Poilus <t@lespoilus.net>
  * Simplify sending mails
  *
