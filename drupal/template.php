5cd6c2b56fe01e4a35876cc3956514ad1d29c9f3
Thierry Poinot <poinot.thierry@gmail.com>
2013-07-30 12:39:23 +0200

    Update template.php

--- drupal/template.php
+++ drupal/template.php
@@ -21,3 +21,26 @@ function lpu_print_block($module, $delta, $render = false){
     if(!$render) return $build;
     return drupal_render($build);
 }
+
+/**
+ * Return value or safe_value of a field
+ * @param string $entity_type : node, user, taxonomy_term, file
+ * @param object $entity : the entity object
+ * @param string $field_name: The field name to get value
+ * @param string $langcode : the lang_code, default NULL
+ * @param boolean $safe_value : return the safe value or the value, default true
+ * @param boolean $full : return the entire item, with the format, safe_value and value, default false, override the $safe_value param
+ * @use field_get_items()
+ */
+function _field_get_value($entity_type, $entity, $field_name, $langcode = NULL, $index = 0, $safe_value = true, $full = false) {
+    $items = field_get_items($entity_type, $entity, $field_name, $langcode);
+    if (array_key_exists($index, $items)) {
+        $item = $items[$index];
+        if ($full === true) {
+            return $item;
+        }
+        if ($safe_value === false) {
+            return $item['value'];
+        }
+    }
+}
